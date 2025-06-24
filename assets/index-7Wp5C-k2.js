e6db083bde0514ff363fad43045b0b4adcea48f6
Amaury Diaz <amauryuh@gmail.com>
2025-06-24 11:01:17 +0200

    Updates

--- assets/index-7Wp5C-k2.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./features-animation-YFRM2BRE.js";import"./index-BBiRo_uk.js";var d=a;export{d as default};
---
+++ assets/index-BKPdp9N1.js
@@ -0,0 +1,1 @@
+import{d as a}from"./features-animation-uzsR1MAc.js";import"./index-Dux2iNQi.js";var d=a;export{d as default};
