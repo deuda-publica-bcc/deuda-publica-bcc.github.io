fbaba4694bf3226e369786b992246d54169591da
Amaury Diaz <amauryuh@gmail.com>
2025-10-24 16:13:04 +0200

    Updates

--- assets/index-C2_Y22lJ.js
+++
@@ -1,1 +0,0 @@
-import{O as a}from"./features-animation-BhAJj5v_.js";import"./index-BPiDIeGX.js";var i=a;export{i as default};
---
+++ assets/src-UW24ZMRV-DztQs86k.js
@@ -0,0 +1,1 @@
+import{O as a}from"./features-animation-BRS0UD1l.js";import"./index-B5kOCIlv.js";var t=a;export{t as default};
